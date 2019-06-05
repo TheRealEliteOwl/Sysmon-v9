fead90f41fe23824dfe90a398ff570cdffb72ed0
TheRealEliteOwl <j.green@onlineowls.com>
2019-06-05 16:57:22 -0400

    Update Auto_Update.bat

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/TheRealEliteOwl/Sysmon-v9/sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/TheRealEliteOwl/Sysmon-v9/master/sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig.xml')"
 sysmon64 -c sysmonconfig.xml
 exit
